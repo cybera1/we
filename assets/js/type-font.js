4a2fb85c8a8d746fc8afbe5ce21d53f83ab599e8
cybera1 <82648439+cybera1@users.noreply.github.com>
2021-12-27 01:29:32 +0530

    Add files via upload

---
+++ assets/js/type-font.js
@@ -0,0 +1,9 @@
+var typed = new Typed('.type', {
+    strings: ['SECURE THE DIGITAL',
+        'BE A CYBER SECURITY EXEPERT',
+        'BE A NETWORK SECURITY ENGINEER'
+    ],
+    typeSpeed: 30,
+    backSpeed: 40,
+    loop: true
+});
